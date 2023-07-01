08e718a649711ca2c1c87cfe2090d544f63c52e4
M. Enes Celik <m96celik@gmail.com>
2023-06-30 18:16:38 -0700

    Level 1 & Level 2 Auth added

--- app.js
+++ app.js
@@ -13,21 +13,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-
 app.get('/', (req, res) => {
     res.render('home');
 })
 
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-
-
-// Register
+// Register Routes
 app.get('/register', (req, res) => {
-    res.render('register');
+    res.render('register', { errMessage: '' });
 });
-
 app.post('/register', (req, res) => {
 
     const userData = req.body;
@@ -42,23 +35,40 @@ app.post('/register', (req, res) => {
         newUser.save()
             .then(savedUser => {
                 console.log('User saved:', savedUser);
-                res.render('secrets'); // Or send a success response
+                res.render('secrets');
             })
-            .catch(error => {
-                console.error('Error saving user:', error);
-                res.render('register', { errMessage: 'Error saving user' }); // Or send an error response
+            .catch(err => {
+                console.error('Error saving user:', err);
             });
     } else {
-        res.render('register', {errMessage: "Password do not match!"})
+        res.render('register', { errMessage: "Password do not match!" })
     }
-
 });
 
 
+// Login Routes
+app.get('/login', (req, res) => {
+    res.render('login', { errMessage: '' });
+});
+app.post('/login', (req, res) => {
 
+    const userEmail = req.body.userEmail;
+    const userPassword = req.body.userPassword;
 
-
-
+    User.findOne({ email: userEmail })
+        .then(foundUser => {
+            if (foundUser.password === userPassword) {
+                res.render('secrets');
+            } else {
+                res.render('login', { errMessage: 'Email or Passowrd is wrong, please try again!' })
+                console.log(foundUser.password);
+            }
+        })
+        .catch(err => {
+            console.log('Error: ' + err);
+        });
+});
+ 
 
 
 
--- db.js
+++ db.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');  
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/usersDB', {useNewUrlParser: true});
 
 
-const userChema = mongoose.Schema({
+const userChema = new mongoose.Schema({
     name: String, 
     email: String,
     password: String
 });
 
+const secret = 'mylongsecret';
+userChema.plugin(encrypt, {secret: secret, excludeFromEncryption: ['name', 'email']});
 
 const User = mongoose.model('User', userChema);
 
